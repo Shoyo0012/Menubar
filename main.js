97ed7fa5ff805933461c080846200e2dc1689efc
Shoyo0012 <sayelhossen041@gmail.com>
2024-09-06 23:31:00 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggleMenu(displaystate) {
+ const nav = document.getElementById('headernavigation');
+ const closeIco = document.getElementById('close-ico');
+
+ nav.style.display = displaystate;
+ closeIco.style.display = displaystate;
+
+}
